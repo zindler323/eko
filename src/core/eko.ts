369d60b8e0b52e099022ae18a0efbcb4fc4076fb
HairlessVillager <hairlessvillager@qq.com>
2025-01-23 14:22:43 +0800

    remove redundant `[]`

--- src/core/eko.ts
+++ src/core/eko.ts
@@ -111,7 +111,6 @@ export class Eko {
     let context = {
       llmProvider: this.llmProvider,
       variables: new Map<string, unknown>(),
-      tabs: [],
       tools: new Map<string, Tool<any, any>>(),
       callback,
     };
